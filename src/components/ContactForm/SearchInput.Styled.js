64205e9bd191a1e97216c8a64febfd338d12a828
ImVladislav <nafanya102@gmail.com>
2023-01-10 20:19:59 +0200

    Update SearchInput.Styled.js

--- src/components/ContactForm/SearchInput.Styled.js
+++ src/components/ContactForm/SearchInput.Styled.js
@@ -1,15 +1,15 @@
 import { Field } from 'formik';
 import styled from 'styled-components';
 
-export const SearchInput = styled.input`
-  width: 300px;
-  font-size: ${p => p.theme.fontSizes.s}px;
-  padding: ${p => p.theme.space[3]}px;
-  :focus {
-    border-color: ${p => p.theme.colors.primary};
-    outline: none;
-  }
-`;
+// export const SearchInput = styled.input`
+//   width: 300px;
+//   font-size: ${p => p.theme.fontSizes.s}px;
+//   padding: ${p => p.theme.space[3]}px;
+//   :focus {
+//     border-color: ${p => p.theme.colors.primary};
+//     outline: none;
+//   }
+// `;
 
 export const Input = styled(Field)`
   width: 300px;
